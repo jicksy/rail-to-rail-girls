f2eef62f0c27984ce976cb5c04d82112f668b466
Aman King <amanking@gmail.com>
2015-01-31 17:09:55 +0530

    added event table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150131101522) do
+ActiveRecord::Schema.define(version: 20150131113837) do
+
+  create_table "events", force: true do |t|
+    t.string   "name"
+    t.string   "location"
+    t.date     "event_date"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
